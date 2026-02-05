9857d848045ea60b0f3ce59ce864d39e35f3b244
Sonal Prasad <sonalprasad.sp@gmail.com>
2026-02-04 17:49:49 -0800

    162 and 287

---
+++ 162. Find Peak Element.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        n = len(nums)
+        l, r = 0, n - 1
+        while l < r:
+            mid = l + (r - l)//2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        return l
